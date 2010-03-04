094ceedf6b302516e9fb5f5ff3b9475d2965ded7
John Trupiano <jtrupiano@gmail.com>
2010-03-04 09:34:58 -0500

    Add colors option for charts

--- lib/google_chart/modules/color.rb
+++ lib/google_chart/modules/color.rb
@@ -1,14 +1,19 @@
 module GoogleChart
   # This module is used as a mixin for providing color functionality
   module Color
+
+    def colors(colors)
+      @colors = colors
+    end
+
     private
     def color(color)
       @colors ||= []
       @colors << color
     end
-
+    
     def add_colors
-      @params[:chco] = @colors.compact.join(",") unless @colors.compact.empty?
+      @params[:chco] = @colors.compact.join("|") unless @colors.compact.empty?
     end
 
   end
